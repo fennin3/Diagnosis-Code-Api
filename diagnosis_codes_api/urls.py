44de9aa0cf19156421250286bc13759d551e7b04
Francis Ennin <enninfrancis47@gmail.com>
2022-04-28 23:30:32 +0000

    Added urls for swagger and redoc documentations

--- diagnosis_codes_api/urls.py
+++ diagnosis_codes_api/urls.py
@@ -1,9 +1,26 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi 
+
+schema_view = get_schema_view(
+openapi.Info(
+    title="Diagnosis Codes API",
+    default_version="v1",
+    description="Well Documented API for Diagnosis Codes",
+    terms_of_service="",
+    contact=openapi.Contact(email="enninfrancis47@gmail.com"),
+    license=openapi.License(name="MIT License"),
+    ),
+public=True,
+permission_classes=(permissions.AllowAny,)
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('diagnosis_code.urls'))
+    path('api/', include('diagnosis_code.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
