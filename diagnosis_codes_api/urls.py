d33cd2055f18bd5e52c71e5fd2905f2aa00f6b8b
Francis Ennin <enninfrancis47@gmail.com>
2022-04-28 22:51:06 +0000

    Updated urls.py

--- diagnosis_codes_api/urls.py
+++ diagnosis_codes_api/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/codes/', include('api_v1.urls'))
+    path('api/', include('diagnosis_code.urls'))
 ]
